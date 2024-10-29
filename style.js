93e4134c0286e2551e77adc5bfdc3bc9bb852dd3
mohdnasir111 <122935164+mohdnasir111@users.noreply.github.com>
2024-10-30 00:27:43 +0530

    Update style.js

--- style.js
+++ style.js
@@ -25,7 +25,7 @@ const getData = async(input) => {
     divs.addEventListener("click",function(){
       window.open(article.url);
     })
-   
+})
   /*let divs = document.createElement("div");
   divs.classList.add("card");
   cardData.appendChild(divs);
@@ -34,10 +34,10 @@ const getData = async(input) => {
         <h3>${jsonData.articles[0].title}</h3>
         <p>${jsonData.articles[0].description}</p>
         `*/
+}
 window.addEventListener("load", function(){
   getData('india');
 })
-  }
 SearchBtn.addEventListener("click", function(){
 let inputText = inputData.value;
 getData(inputText);
@@ -59,3 +59,4 @@ function navClick(navName){
     document.getElementById("technology").style.color="rgb(0,140,255)"
   }
  getData(navName); 
+}
